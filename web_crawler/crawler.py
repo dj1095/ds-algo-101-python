43a3747b3455dd5fe4444b54f86d09cb637f8b50
pramodh <durgapramodh10dj95@gmail.com>
2022-07-30 02:47:10 -0500

    emails from url implemented

--- web_crawler/crawler.py
+++ web_crawler/crawler.py
@@ -1,18 +1,31 @@
 import re
+from urllib.request import urlopen
 
 
-def fetch_emails_from_doc(file_name):
+def fetch_distinct_emails(content):
     distinct_mails = []
+    mails = re.findall('[a-zA-Z0-9]+@[a-zA-Z]+.[a-zA-Z]+', content)
+    [distinct_mails.append(m) for m in mails if m not in distinct_mails]
+    return distinct_mails
+
+
+def fetch_emails_from_doc(file_path):
     try:
-        file_content = open(file_name).read()
-        mails = re.findall('[a-zA-Z0-9]+@[a-zA-Z]+.[a-zA-Z]+', file_content)
-        [distinct_mails.append(m) for m in mails if m not in distinct_mails]
+        file_content = open(file_path).read()
     except FileNotFoundError:
         print('File Not Found')
     except:
         print('Error Occurred!!!')
-    return distinct_mails
+    return fetch_distinct_emails(file_content)
 
 
-emails = fetch_emails_from_doc('Lab6_Webpage.html')
-print(f'Distinct emails in the document : {emails}')
+def fetch_emails_from_url(url):
+    content = urlopen(url).read().decode()
+    return fetch_distinct_emails(content)
+
+
+emails_doc = fetch_emails_from_doc('Lab6_Webpage.html')
+print(f'Distinct emails in the document : {emails_doc}')
+
+emails_url = fetch_emails_from_url(' http://e-mailid.blogspot.com/')
+print(f'Distinct emails in the url : {emails_url}')
